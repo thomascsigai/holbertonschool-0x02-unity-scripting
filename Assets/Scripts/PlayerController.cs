b55a4155b8bc03f107f0b8978ea6f085c47804b0
Thomas Csigai <thomascsigai1@gmail.com>
2024-01-16 15:49:45 +0100

    Adding character movement + coin prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 10.0f;
+    private PlayerControls controls;
+    private Vector2 move;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        rb = GetComponent<Rigidbody>();
+
+        if(rb == null)
+        {
+            Debug.LogError("Rigidbody is NULL");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        move = controls.movementActionMap.Move.ReadValue<Vector2>();
+        Vector3 movement = new Vector3(move.x, 0.0f, move.y);
+        rb.velocity = movement * speed;
+    }
+
+    private void OnEnable()
+    {
+        controls.movementActionMap.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.movementActionMap.Disable();
+    }
+}
